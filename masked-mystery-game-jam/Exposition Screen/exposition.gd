e3426dcb2d2b3d8d7462798b4571abf20dd17817
Dayna Clare <daycanimate@gmail.com>
2026-02-07 01:12:39 -0500

    adjusted exposition screen

--- masked-mystery-game-jam/Exposition Screen/exposition.gd
+++ masked-mystery-game-jam/Exposition Screen/exposition.gd
@@ -1,30 +1,51 @@
 extends Node2D
 
 @onready var label = $ExpositionBox/RichTextLabel
-@onready var button = $Start
+@onready var start_button = $Start
+@onready var back_button = $Back
+@onready var next_button = $Next
+@onready var skip_button = $Skip
+
 var count = 1
 
 func _ready() -> void:
-	button.visible = false
+	start_button.visible = false
 
 func _physics_process(delta: float) -> void:
+	if count != 1 || count != 8:
+		back_button.visible = true
+		next_button.visible = true
+	
 	if count == 1:
+		back_button.visible = false
+		
 		label.text = "You’re a detective working at your own agency."
+		
 	elif count == 2:
 		label.text = "You’ve recently gotten a strange case at your doorstep. "
+		
 	elif count == 3:
 		label.text = "A masquerade party has ended in tragedy. "
+		
 	elif count == 4:
 		label.text = "The victim hosted the party and was viciously murdered at the scene."
+	
 	elif count == 5:
 		label.text = "You have 3 suspects, each of which has provided their statements on where they were around the time of the murder. "
+	
 	elif count == 6:
 		label.text = "You know where the murder likely took place. It’s up to you to solve the mystery."
+	
 	elif count == 7:
 		label.text = "You have 3 masks belonging to each of the suspects. Explore the crime scene from each of their perspectives by putting on each mask."
+	
 	elif count == 8:
+		next_button.visible = false
+		
 		label.text = "Use these perspectives to solve the mystery in time before the culprit escapes!"
-		button.visible = true
+		
+		start_button.visible = true
+		skip_button.visible = false
 
 
 func _on_back_pressed() -> void:
