16ae7a74a340b94f173e626afc63d4ea54701387
Dayna Clare <daycanimate@gmail.com>
2026-02-08 00:17:44 -0500

    fixed dropdowns in deduction screen

--- masked-mystery-game-jam/Deduction Screen/deduction.gd
+++ masked-mystery-game-jam/Deduction Screen/deduction.gd
@@ -1,16 +1,17 @@
 extends Node2D
 
-@onready var ques1 = $Question_1/OptionButton
-@onready var ques2 = $Question_2/OptionButton
-@onready var ques3 = $Question_3/OptionButton
+@onready var q1_dropdrown = $Question_1/OptionButton
+@onready var q2_dropdrown = $Question_2/OptionButton
+@onready var q3_dropdrown = $Question_3/OptionButton
 
-
-func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://Investigation Screen/Scenes/investigation.tscn")
 
 func _on_texture_button_pressed() -> void:
-	print("This is 1: ", ques1.get_selected_id()," This is 2:", ques2.get_selected_id(), " This is 3:" , ques3.get_selected_id())
-	if ques1.get_selected_id() == 1 and ques2.get_selected_id() == 3 and ques3.get_selected_id() == 2:
+	
+	var q1_answer = q1_dropdrown.get_selected_id()
+	var q2_answer = q2_dropdrown.get_selected_id()
+	var q3_answer = q3_dropdrown.get_selected_id()
+	
+	if  q1_answer == 1 and q2_answer == 3 and q3_answer == 2:
 		get_tree().change_scene_to_file("res://Outro Screen/Scenes/outro_win.tscn")
 	else:
 		get_tree().change_scene_to_file("res://Outro Screen/Scenes/outro_lose.tscn")
